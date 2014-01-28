80bebcc06340e10ae2e152a3f04845871ffac714
Nathan Thiesen <nathan.thiesen@outlook.com>
2014-01-28 18:05:39 -0200

    test, tests, testes

--- spec/lib/zauberflote/person_spec.rb
+++ spec/lib/zauberflote/person_spec.rb
@@ -11,34 +11,37 @@ describe Zauberflote::Person do
   end
   describe "initialize(instance, params)" do
     before do
-      @instance = Zauberflote::Instance.new('https://nathanthiesen.highrisehq.com/', '9f3e6a2786c7affce261b75c2f256463')
-      @person = @instance.create_person({:name => "foo", :unknown => "bar"})
+      instance = Zauberflote::Instance.new('https://nathanthiesen.highrisehq.com/', '9f3e6a2786c7affce261b75c2f256463')
+      @person = instance.create_person({:name => "foo", :unknown => "bar"})
     end
-    it 'must receive the params argument' do
+    it 'should receive the params argument' do
       @person.name.must_equal "foo"
     end
-    it 'must ignore the unknown key' do
+    it 'should ignore the unknown key' do
       lambda {@person.unknown}.must_raise NoMethodError
     end
   end
   describe 'highrise_save' do
     before do
-     @instance = Zauberflote::Instance.new('https://nathanthiesen.highrisehq.com/', '9f3e6a2786c7affce261b75c2f256463')
-     @person = @instance.create_person({:name => "foo"})
-     @person_nil = @instance.create_person
-     @save = @person.highrise_save
-     @save_nil = @person_nil.highrise_save
+     instance = Zauberflote::Instance.new('https://nathanthiesen.highrisehq.com/', '9f3e6a2786c7affce261b75c2f256463')
+     person = instance.create_person({:name => "foo"})
+     person_nil = instance.create_person
+     save = person.highrise_save
+     save_nil = person_nil.highrise_save
     end
   end
 
   describe 'saving people in concurrent instances' do
     before do
-      @instance_a = Zauberflote::Instance.new('https://self1369.highrisehq.com/', '5bfa2ad349f174764a333b5eea8730cb')
-      @instance_b = Zauberflote::Instance.new('https://nathanthiesen.highrisehq.com/', '9f3e6a2786c7affce261b75c2f256463')
-      @person_a = @instance_a.create_person({:name => "InstanceA"})
-      @person_b = @instance_b.create_person({:name => "InstanceB"})
+      instance_a = Zauberflote::Instance.new('https://self1369.highrisehq.com/', '5bfa2ad349f174764a333b5eea8730cb')
+      instance_b = Zauberflote::Instance.new('https://nathanthiesen.highrisehq.com/', '9f3e6a2786c7affce261b75c2f256463')
+      person_a = instance_a.create_person({:name => "InstanceA"})
+      person_b = instance_b.create_person({:name => "InstanceB"})
     end
 
+    it 'should return an id' do
+      lambda { person_a.highrise_save}.wont_nil
+    end
 
   end
 
